7db521dd98c8b6cddc42d4a5f3da849a442c1b25
Devansh Ramdurgekar <devanshr3012@gmail.com>
2025-05-14 15:47:08 +0530

    Update barometer.cpp

--- esp32s3/Sensors/barometer.cpp
+++ esp32s3/Sensors/barometer.cpp
@@ -1,54 +1,71 @@
-#include "barometer.h"
+
+/***************************************************************************
+  This is a library for the BMP3XX temperature & pressure sensor
+
+  Designed specifically to work with the Adafruit BMP388 Breakout
+  ----> http://www.adafruit.com/products/3966
+
+  These sensors use I2C or SPI to communicate, 2 or 4 pins are required
+  to interface.
+
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit and open-source hardware by purchasing products
+  from Adafruit!
+
+  Written by Limor Fried & Kevin Townsend for Adafruit Industries.
+  BSD license, all text above must be included in any redistribution
+ ***************************************************************************/
+
 #include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_Sensor.h>
+#include "Adafruit_BMP3XX.h"
 
-Barometer::Barometer() {}
+#define BMP_SCK 13
+#define BMP_MISO 12
+#define BMP_MOSI 11
+#define BMP_CS 10
 
-void Barometer::init() {
-    Wire.begin();
-    // Initialize your barometer here
-}
+#define SEALEVELPRESSURE_HPA (1013.25)
 
-float Barometer::getAltitude() {
-    // Return the altitude in meters
-    return 100.0;  // Dummy value
-}
-
-float Barometer::getPressure() {
-    // Return the pressure in Pascals
-    return 101325.0;  // Dummy value
-}
-
-float Barometer::getTemperature() {
-    // Return the temperature in Celsius
-    return 25.0;  // Dummy value
-}
+Adafruit_BMP3XX bmp;
 
 void setup() {
-    Serial.begin(115200);
-    Barometer barometer;
-    barometer.init();
+  Serial.begin(115200);
+  while (!Serial);
+  Serial.println("Adafruit BMP388 / BMP390 test");
+
+  if (!bmp.begin_I2C()) {   // hardware I2C mode, can pass in address & alt Wire
+  //if (! bmp.begin_SPI(BMP_CS)) {  // hardware SPI mode  
+  //if (! bmp.begin_SPI(BMP_CS, BMP_SCK, BMP_MISO, BMP_MOSI)) {  // software SPI mode
+    Serial.println("Could not find a valid BMP3 sensor, check wiring!");
+    while (1);
+  }
+
+  // Set up oversampling and filter initialization
+  bmp.setTemperatureOversampling(BMP3_OVERSAMPLING_8X);
+  bmp.setPressureOversampling(BMP3_OVERSAMPLING_4X);
+  bmp.setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_3);
+  bmp.setOutputDataRate(BMP3_ODR_50_HZ);
 }
 
 void loop() {
-    Barometer barometer;
-    
-    // Read sensor data
-    float altitude = barometer.getAltitude();
-    float pressure = barometer.getPressure();
-    float temperature = barometer.getTemperature();
-    
-    // Print the sensor data
-    Serial.print("Altitude: ");
-    Serial.print(altitude);
-    Serial.println(" m");
-    
-    Serial.print("Pressure: ");
-    Serial.print(pressure);
-    Serial.println(" Pa");
-    
-    Serial.print("Temperature: ");
-    Serial.print(temperature);
-    Serial.println(" C");
-    
-    delay(1000);  // Adjust delay as needed
+  if (! bmp.performReading()) {
+    Serial.println("Failed to perform reading :(");
+    return;
+  }
+  Serial.print("Temperature = ");
+  Serial.print(bmp.temperature);
+  Serial.println(" *C");
+
+  Serial.print("Pressure = ");
+  Serial.print(bmp.pressure / 100.0);
+  Serial.println(" hPa");
+
+  Serial.print("Approx. Altitude = ");
+  Serial.print(bmp.readAltitude(SEALEVELPRESSURE_HPA));
+  Serial.println(" m");
+
+  Serial.println();
+  delay(2000);
 }
