3fe6a976d8c41c62d36c61e4214db803055fc34e
Devansh Ramdurgekar <devanshr3012@gmail.com>
2025-05-14 15:48:43 +0530

    Update barometer.cpp

--- esp32s3/Sensors/barometer.cpp
+++ esp32s3/Sensors/barometer.cpp
@@ -1,71 +1,55 @@
-
-/***************************************************************************
-  This is a library for the BMP3XX temperature & pressure sensor
-
-  Designed specifically to work with the Adafruit BMP388 Breakout
-  ----> http://www.adafruit.com/products/3966
-
-  These sensors use I2C or SPI to communicate, 2 or 4 pins are required
-  to interface.
-
-  Adafruit invests time and resources providing this open source code,
-  please support Adafruit and open-source hardware by purchasing products
-  from Adafruit!
-
-  Written by Limor Fried & Kevin Townsend for Adafruit Industries.
-  BSD license, all text above must be included in any redistribution
- ***************************************************************************/
-
 #include <Wire.h>
-#include <SPI.h>
 #include <Adafruit_Sensor.h>
-#include "Adafruit_BMP3XX.h"
+#include <Adafruit_BMP3XX.h>
 
-#define BMP_SCK 13
-#define BMP_MISO 12
-#define BMP_MOSI 11
-#define BMP_CS 10
-
-#define SEALEVELPRESSURE_HPA (1013.25)
-
+// Create an instance of the sensor
 Adafruit_BMP3XX bmp;
+
+// Optional: define custom I2C pins for ESP32
+#define I2C_SDA 8
+#define I2C_SCL 9
 
 void setup() {
   Serial.begin(115200);
-  while (!Serial);
-  Serial.println("Adafruit BMP388 / BMP390 test");
+  while (!Serial) delay(10);
 
-  if (!bmp.begin_I2C()) {   // hardware I2C mode, can pass in address & alt Wire
-  //if (! bmp.begin_SPI(BMP_CS)) {  // hardware SPI mode  
-  //if (! bmp.begin_SPI(BMP_CS, BMP_SCK, BMP_MISO, BMP_MOSI)) {  // software SPI mode
-    Serial.println("Could not find a valid BMP3 sensor, check wiring!");
+  Serial.println("BMP390 test");
+
+  // Initialize I2C with custom pins (ESP32 only)
+  Wire.begin(I2C_SDA, I2C_SCL);
+
+  // Begin BMP390 using I2C
+  if (!bmp.begin_I2C()) {
+    Serial.println("Could not find a valid BMP390 sensor, check wiring!");
     while (1);
   }
 
-  // Set up oversampling and filter initialization
+  // Set oversampling and filter for better accuracy
   bmp.setTemperatureOversampling(BMP3_OVERSAMPLING_8X);
   bmp.setPressureOversampling(BMP3_OVERSAMPLING_4X);
   bmp.setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_3);
-  bmp.setOutputDataRate(BMP3_ODR_50_HZ);
+  bmp.setOutputDataRate(BMP3_ODR_50_HZ); // Optional
 }
 
 void loop() {
-  if (! bmp.performReading()) {
+  if (!bmp.performReading()) {
     Serial.println("Failed to perform reading :(");
+    delay(500);
     return;
   }
+
   Serial.print("Temperature = ");
   Serial.print(bmp.temperature);
   Serial.println(" *C");
 
   Serial.print("Pressure = ");
-  Serial.print(bmp.pressure / 100.0);
+  Serial.print(bmp.pressure / 100.0); // Convert to hPa
   Serial.println(" hPa");
 
-  Serial.print("Approx. Altitude = ");
-  Serial.print(bmp.readAltitude(SEALEVELPRESSURE_HPA));
+  Serial.print("Approx Altitude = ");
+  Serial.print(bmp.readAltitude(1013.25)); // Adjust sea-level pressure as needed
   Serial.println(" m");
 
   Serial.println();
-  delay(2000);
+  delay(1000);
 }
