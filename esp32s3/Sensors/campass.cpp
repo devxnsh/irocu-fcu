669616a1168f67f0aa5e04a170e11b30b43db4a3
Shreeveerpatel <167593235+Shreeveerpatel@users.noreply.github.com>
2025-05-17 13:26:00 +0530

    Update and rename compass.cpp to campass.cpp

---
+++ esp32s3/Sensors/campass.cpp
@@ -0,0 +1,125 @@
+#include "bmm150.h"
+#include "bmm150_defs.h"
+
+BMM150 bmm = BMM150();
+bmm150_mag_data value;
+bmm150_mag_data value_offset;
+float Heading[3];
+float Degrees[3];
+
+void init_campass()
+{
+
+  if(bmm.initialize() == BMM150_E_ID_NOT_CONFORM) {
+    Serial.println("Chip ID can not read!");
+    while(1);
+  } else {
+    Serial.println("Initialize done!");
+  }
+}
+/**
+ * @brief Do figure-8 calibration for a limited time to get offset values of x/y/z axis.
+ * @param timeout - seconds of calibration period.
+*/
+void campass_calibrate(uint32_t timeout)
+{
+
+  Serial.println("Start figure-8 calibration after 3 seconds.");
+  delay(3000);
+  Serial.print("\n\rCalibrate done..");
+
+  int16_t value_x_min = 0;
+  int16_t value_x_max = 0;
+  int16_t value_y_min = 0;
+  int16_t value_y_max = 0;
+  int16_t value_z_min = 0;
+  int16_t value_z_max = 0;
+  uint32_t timeStart = 0;
+
+  bmm.read_mag_data();  
+  value_x_min = bmm.raw_mag_data.raw_datax;
+  value_x_max = bmm.raw_mag_data.raw_datax;
+  value_y_min = bmm.raw_mag_data.raw_datay;
+  value_y_max = bmm.raw_mag_data.raw_datay;
+  value_z_min = bmm.raw_mag_data.raw_dataz;
+  value_z_max = bmm.raw_mag_data.raw_dataz;
+  delay(100);
+
+  timeStart = millis();
+  
+  while((millis() - timeStart) < timeout)
+  {
+    bmm.read_mag_data();
+    
+    /* Update x-Axis max/min value */
+    if(value_x_min > bmm.raw_mag_data.raw_datax)
+    {
+      value_x_min = bmm.raw_mag_data.raw_datax;
+      // Serial.print("Update value_x_min: ");
+      // Serial.println(value_x_min);
+
+    } 
+    else if(value_x_max < bmm.raw_mag_data.raw_datax)
+    {
+      value_x_max = bmm.raw_mag_data.raw_datax;
+      // Serial.print("update value_x_max: ");
+      // Serial.println(value_x_max);
+    }
+
+    /* Update y-Axis max/min value */
+    if(value_y_min > bmm.raw_mag_data.raw_datay)
+    {
+      value_y_min = bmm.raw_mag_data.raw_datay;
+      // Serial.print("Update value_y_min: ");
+      // Serial.println(value_y_min);
+
+    } 
+    else if(value_y_max < bmm.raw_mag_data.raw_datay)
+    {
+      value_y_max = bmm.raw_mag_data.raw_datay;
+      // Serial.print("update value_y_max: ");
+      // Serial.println(value_y_max);
+    }
+
+    /* Update z-Axis max/min value */
+    if(value_z_min > bmm.raw_mag_data.raw_dataz)
+    {
+      value_z_min = bmm.raw_mag_data.raw_dataz;
+      // Serial.print("Update value_z_min: ");
+      // Serial.println(value_z_min);
+
+    } 
+    else if(value_z_max < bmm.raw_mag_data.raw_dataz)
+    {
+      value_z_max = bmm.raw_mag_data.raw_dataz;
+      // Serial.print("update value_z_max: ");
+      // Serial.println(value_z_max);
+    }
+    
+    Serial.print(".");
+    delay(100);
+  }
+  value_offset.x = value_x_min + (value_x_max - value_x_min)/2;
+  value_offset.y = value_y_min + (value_y_max - value_y_min)/2;
+  value_offset.z = value_z_min + (value_z_max - value_z_min)/2;
+}
+
+int campass_reading() {
+  bmm.read_mag_data();
+  value.x = bmm.raw_mag_data.raw_datax - value_offset.x;
+  value.y = bmm.raw_mag_data.raw_datay - value_offset.y;
+  value.z = bmm.raw_mag_data.raw_dataz - value_offset.z;
+
+  Heading[1] = atan2(value.x, value.y);
+  Heading[2] = atan2(value.z, value.x);
+  Heading[0] = Heading[2];
+
+  if (Heading[0] < 0) Heading[0] += 2 * PI;
+  if (Heading[0] > 2 * PI) Heading[0] -= 2 * PI;
+
+  Degrees[0] = Heading[0] * 180 / M_PI;
+  Degrees[1] = Heading[1] * 180 / M_PI;
+  Degrees[2] = Heading[2] * 180 / M_PI;
+
+  return (int)Degrees[0]; // Or Degrees[1], depending on what you need
+}
